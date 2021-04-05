c2df9bc92c67e3e4f9837e602183409b6efa027a
carmen-ho <75396980+carmen-ho@users.noreply.github.com>
2021-04-05 04:13:17 -0700

    update final files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,35 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// var table = d3.select('table');
+var tbody = d3.select('tbody');
+
+function buildtable(data) {
+
+    tbody.html("");
+
+    data.forEach(ufo => {
+        var addRow = tbody.append('tr');
+    Object.values(ufo).forEach((value) => {
+        var cell = addRow.append("td");
+        cell.text(value);
+     });
+    });
+}
+
+function click() {
+
+    var dateinput = d3.select("#datetime").property("value");
+
+    var filteredData = tableData;
+    if (dateinput){
+        filteredData = filteredData.filter(addRow => addRow.datetime === dateinput);
+    }
+
+    buildtable(filteredData);
+}
+    
+d3.selectAll("#filter-btn").on("click",click);
+
+buildtable(tableData);
+
